3ab9ea04904e2952e6c4c207b656e867c17d759f
秦臻 <qinzhen@heywoods.cn>
2020-01-08 22:41:42 +0800

    qz commit

--- xhw.config.js
+++ xhw.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  cliPath: "D:\\wechat_devtool\\cli",
-  proxyTable: {},
-  mockPort: 3000,
+  cliPath: "E:\\微信web开发者工具\\cli",  //微信开发者工具cli所在目录
+  proxyTable: {},  // 代理设置
+  mockPort: 3000, // mock数据的端口
 }
 
 
