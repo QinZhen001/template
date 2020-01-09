3daecf6282b1a4319285cd312aeb6ba6ba62665a
秦臻 <qinzhen@heywoods.cn>
2020-01-09 16:05:31 +0800

    整理命令

--- xhw.config.js
+++ xhw.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  cliPath: "E:\\微信web开发者工具\\cli",  //微信开发者工具cli所在目录
+  toolPath: "E:\\微信web开发者工具",  //微信开发者工具cli所在目录
   proxyTable: {},  // 代理设置
   mockPort: 3000, // mock数据的端口
 }
